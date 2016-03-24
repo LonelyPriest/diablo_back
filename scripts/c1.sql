c90f09b2b9e9a81d0390cca703830a799c6cd8a0
buxianhui <47396648@qq.com>
2016-03-25 00:48:44 +0800

    fit bugs

--- scripts/c1.sql
+++
@@ -1,6 +0,0 @@
-insert into w_retailer(name, balance, mobile, merchant, change_date, entry_date) values('张三1', 5, '13773738118', 11, now(), curdate());
-insert into w_retailer(name, balance, mobile, merchant, change_date, entry_date) values('张三2', 6, '13773738119', 11, now(), curdate());
-insert into w_retailer(name, balance, mobile, merchant, change_date, entry_date) values('张三3', 7, '13773738120', 11, now(), curdate());
-insert into w_retailer(name, balance, mobile, merchant, change_date, entry_date) values('张三4', 8, '', 11, now(), curdate());
-insert into w_retailer(name, balance, mobile, merchant, change_date, entry_date) values('张三5', 9, '13773738122', 11, now(), curdate());
-insert into w_retailer(name, balance, mobile, merchant, change_date, entry_date) values('张三6', 0, '13773738122', 11, now(), curdate());
